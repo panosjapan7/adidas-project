ef73b9c69893f6591c621d5348741c6dcab5d2bf
panosjapan7 <panosjapan@gmail.com>
2023-07-25 18:24:44 +0200

    Creates child components with basic code

---
+++ src/sections/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../App.css";
+
+const Home = () => {
+  return (
+    <section id="homeSection" className="homeSection">
+      <h2>Home</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+        obcaecati sunt, aliquam at rerum accusantium ea suscipit necessitatibus
+        doloremque vero!
+      </p>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/sections/ProductPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProductPage = ({ myRef }: { myRef: any }) => {
+  return (
+    <section id="productSection" className="productSection">
+      <h2>ProductPage</h2>
+      <p ref={myRef}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt vel
+        minima dicta odio iusto itaque eos voluptates, ipsa libero voluptatem?
+      </p>
+    </section>
+  );
+};
+
+export default ProductPage;
