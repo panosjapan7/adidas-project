1d7b30b842df94fb016d4ecf7f551ba4676d9ac8
panosjapan7 <panosjapan@gmail.com>
2023-07-26 08:51:34 +0200

    Creates arrow icons, vertical line

--- src/components/NavigationArrows.tsx
+++ src/components/NavigationArrows.tsx
@@ -1,8 +1,32 @@
 import React from "react";
+import { SlArrowUp, SlArrowDown } from "react-icons/sl";
 import "./components.css";
 
-const NavigationArrows = () => {
-  return <div className="navivationArrows-wrapper">NavigationArrows</div>;
+const NavigationArrows = ({
+  isVisible,
+}: {
+  isVisible: boolean | undefined;
+}) => {
+  console.log({ isVisible });
+  return (
+    <div className="navivationArrows-wrapper">
+      <div className="navigationArrows-content-wrapper">
+        <div className="arrow-container">
+          <SlArrowUp
+            className="arrow"
+            style={{ color: isVisible ? "black" : "#D3D3D3" }}
+          />
+        </div>
+        <div className="vertical-line"></div>
+        <div className="arrow-container">
+          <SlArrowDown
+            className="arrow"
+            style={{ color: isVisible ? "#D3D3D3" : "black" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavigationArrows;
