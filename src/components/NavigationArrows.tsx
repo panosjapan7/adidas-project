5cb85d292e756463cd5bd8f0b97d46c482bdb41a
panosjapan7 <panosjapan@gmail.com>
2023-07-26 08:05:45 +0200

    Creates and centers component

---
+++ src/components/NavigationArrows.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./components.css";
+
+const NavigationArrows = () => {
+  return <div className="navivationArrows-wrapper">NavigationArrows</div>;
+};
+
+export default NavigationArrows;
