9af237dd6da0780fc4abea22b5f2d7977719fecb
panosjapan7 <panosjapan@gmail.com>
2023-07-28 08:07:53 +0200

    Makes Cart Tag draggable & not leaving the edge of screen

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import gsap from "gsap";
+import Draggable from "gsap/Draggable";
 import { BsHandbag } from "react-icons/bs";
 import "./components.css";
 import adidasLogo from "../assets/adidas-logo-white.png";
 
+gsap.registerPlugin(Draggable);
+
 const TopMenu = () => {
+  const [cartHeight, setCartHeight] = useState(175);
+
+  useEffect(() => {
+    Draggable.create(".cart-desktop-container", {
+      type: "y",
+      bounds: {
+        minY: 0,
+        maxY: 1,
+      },
+      edgeResistance: 0.9,
+      onDrag() {
+        this.prevY = this.y;
+        console.log(this.y);
+        if (this.y > 65) {
+          console.log("Dragged Enough!");
+        }
+      },
+      onDragEnd() {
+        gsap.fromTo(
+          ".cart-desktop-container",
+          { y: this.prevY },
+          {
+            duration: 0.5,
+            y: 0,
+            ease: "elastic(1.5, 0.4)",
+          }
+        );
+      },
+    });
+  }, []);
+
   return (
     <div className="menu-wrapper">
       <div className="menu-container">
@@ -17,7 +52,7 @@ const TopMenu = () => {
           <p className="text-links">github</p>
         </a>
       </div>
-      <div className="cart-desktop-container">
+      <div className="cart-desktop-container" style={{ height: cartHeight }}>
         <BsHandbag className="handbag-icon" />
       </div>
     </div>
