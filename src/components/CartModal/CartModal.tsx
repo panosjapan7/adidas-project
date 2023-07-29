baa0e3735b6426eb260705e7993ce59de906c6d5
panosjapan7 <panosjapan@gmail.com>
2023-07-29 08:53:23 +0200

    Creates table with static data

--- src/components/CartModal/CartModal.tsx
+++ src/components/CartModal/CartModal.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import "./cartModal.css";
 import Backdrop from "./Backdrop";
+import ShoeIconBlack from "../../assets/shoe-icon-black.png";
 
 const dropIn = {
   hidden: {
@@ -40,14 +41,68 @@ const CartModal = ({ cartTagDragged }: { cartTagDragged: boolean }) => {
             <p className="header">CART</p>
             <p className="header-quantity">(3 items)</p>
           </div>
-          <div className="cart-sections-container">
-            <p className="one">PRODUCT</p>
-            <p className="two">COLOR</p>
-            <p className="three">PRICE</p>
-            <p className="four">QUANTITY</p>
-            <p className="five">TOTAL</p>
-            <div className="hr"></div>
-          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>{/* Intentionally Blank */}</th>
+                <th>PRODUCT</th>
+                <th>COLOR</th>
+                <th>PRICE</th>
+                <th>QUANTITY</th>
+                <th>TOTAL</th>
+              </tr>
+              <tr className="hr">
+                <td style={{ padding: 10 }}></td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>
+                  <img
+                    src={ShoeIconBlack}
+                    className="shoe-icon"
+                    alt="shoe-icon"
+                  />
+                </th>
+                <td>ultraboost 1.0</td>
+                <td>Black</td>
+                <td>$190.00</td>
+                <td>1</td>
+                <td>$190.00</td>
+              </tr>
+              <tr>
+                <th>
+                  <img
+                    src={ShoeIconBlack}
+                    className="shoe-icon"
+                    alt="shoe-icon"
+                  />
+                </th>
+                <td>ultraboost 1.0</td>
+                <td>Black</td>
+                <td>$190.00</td>
+                <td>1</td>
+                <td>$190.00</td>
+              </tr>
+              <tr>
+                <th className="shoe-column" style={{ paddingBottom: 20 }}>
+                  <img
+                    src={ShoeIconBlack}
+                    className="shoe-icon"
+                    alt="shoe-icon"
+                  />
+                </th>
+                <td style={{ paddingBottom: 20 }}>ultraboost 1.0</td>
+                <td style={{ paddingBottom: 20 }}>Black</td>
+                <td style={{ paddingBottom: 20 }}>$190.00</td>
+                <td style={{ paddingBottom: 20 }}>2</td>
+                <td style={{ paddingBottom: 20 }}>$190.00</td>
+              </tr>
+              <tr className="hr-bottom">
+                <td style={{ paddingTop: 200 }}></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </motion.div>
     </Backdrop>
