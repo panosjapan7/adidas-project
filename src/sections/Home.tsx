0294038b1409d7610959d764742b24a75d6bafb1
panosjapan7 <panosjapan@gmail.com>
2023-08-01 10:59:48 +0200

    Renders shoe hero based on shoeColor state

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import "../assets/styles/home.css";
 import ShoeHeroDesktop from "../assets/images/shoe-hero-desktop.svg";
+import ShoeHeroDesktopPink from "../assets/images/shoe-hero-pink.svg";
+import ShoeHeroDesktopWhite from "../assets/images/shoe-hero-white.svg";
 import ShoeHeroShadowDesktop from "../assets/images/shoe-hero-shadow-desktop.svg";
 
 const Home = ({
   homeSectionRef,
+  shoeColor,
 }: {
   homeSectionRef: React.RefObject<HTMLDivElement>;
+  shoeColor: string;
 }) => {
+  let shoeHeroImage;
+  if (shoeColor === "black") {
+    shoeHeroImage = ShoeHeroDesktop;
+  }
+  if (shoeColor === "pink") {
+    shoeHeroImage = ShoeHeroDesktopPink;
+  }
+  if (shoeColor === "white") {
+    shoeHeroImage = ShoeHeroDesktopWhite;
+  }
+
   return (
     <section id="homeSection" className="homeSection">
       <div className="ultraboost-text-container" ref={homeSectionRef}>
@@ -23,7 +38,7 @@ const Home = ({
 
       <div className="shoe-hero-desktop-container" ref={homeSectionRef}>
         <img
-          src={ShoeHeroDesktop}
+          src={shoeHeroImage}
           className="shoe-hero-desktop"
           alt="adidas ultraboost shoe"
         />
