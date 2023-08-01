73be01ba27bf8ad039356c57bdf5d53db61adfea
panosjapan7 <panosjapan@gmail.com>
2023-08-01 13:35:52 +0200

    Creates left, right column with bg animation

--- src/sections/ProductPage.tsx
+++ src/sections/ProductPage.tsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../assets/styles/productPage.css";
 
 const ProductPage = ({
   productSectionRef,
+  isProductSectionVisible,
 }: {
   productSectionRef: React.RefObject<HTMLDivElement>;
+  isProductSectionVisible: boolean | undefined;
 }) => {
+  useEffect(() => {
+    if (isProductSectionVisible) {
+      const rightColumn = document.querySelector(
+        ".right-column"
+      ) as HTMLDivElement;
+      if (rightColumn) {
+        console.log(rightColumn);
+        rightColumn.classList.add("expanded");
+      }
+    }
+  }, [isProductSectionVisible]);
+
   return (
     <section id="productSection" className="productSection">
+      <div className="product-title-container">
+        <p className="product-title">ultraboost 1.0</p>
+        <p className="product-price">$190.00</p>
+      </div>
       <div className="left-column" ref={productSectionRef} id="productSection">
         <h2>ProductPage</h2>
         <p ref={productSectionRef}>
