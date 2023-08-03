2b61e86d98da4060cf63c951ca3b68d90c4a3b44
panosjapan7 <panosjapan@gmail.com>
2023-08-03 08:37:47 +0200

    Creates pinks slides

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -5,25 +5,32 @@ import BlackS1 from "../assets/images/slides/slide-1-black.png";
 import BlackS2 from "../assets/images/slides/slide-2-black.png";
 import BlackS3 from "../assets/images/slides/slide-3-black.png";
 import BlackS4 from "../assets/images/slides/slide-4-black.png";
+import PinkS0 from "../assets/images/slides/slide-0-pink.png";
+import PinkS1 from "../assets/images/slides/slide-1-pink.png";
+import PinkS2 from "../assets/images/slides/slide-2-pink.png";
+import PinkS3 from "../assets/images/slides/slide-3-pink.png";
+import PinkS4 from "../assets/images/slides/slide-4-pink.png";
+
 import { AiFillLeftCircle, AiFillRightCircle } from "react-icons/ai";
 
 const Slider = () => {
-  const slides = [BlackS0, BlackS1, BlackS2, BlackS3, BlackS4];
+  const slidesBlack = [BlackS0, BlackS1, BlackS2, BlackS3, BlackS4];
+  const slidesPink = [PinkS0, PinkS1, PinkS2, PinkS3, PinkS4];
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const goToNextSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % slidesBlack.length);
   };
 
   const goToPreviousSlide = () => {
     setCurrentSlide((prevSlide) =>
-      prevSlide === 0 ? slides.length - 1 : prevSlide - 1
+      prevSlide === 0 ? slidesBlack.length - 1 : prevSlide - 1
     );
   };
   console.log({ currentSlide });
   return (
     <div className="slider">
-      {slides.map((slide, index) => (
+      {slidesPink.map((slide, index) => (
         <img
           key={index}
           src={slide}
