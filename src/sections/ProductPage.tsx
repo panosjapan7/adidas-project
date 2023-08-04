fcfc67fea2d615c0a8691c28710a590e4f1dfaaa
panosjapan7 <panosjapan@gmail.com>
2023-08-04 12:45:59 +0200

    Removes unused <p> with productSectionRef

--- src/sections/ProductPage.tsx
+++ src/sections/ProductPage.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-
 import "../assets/styles/productPage.css";
 import Slider from "../components/Slider";
 import ProductDetails from "../components/ProductDetails/ProductDetails";
@@ -52,11 +51,6 @@ const ProductPage = ({
       </div>
       <div className="left-column" ref={productSectionRef} id="productSection">
         <Slider shoeColor={shoeColor} />
-
-        {/* <p ref={productSectionRef}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt vel
-          minima dicta odio iusto itaque eos voluptates, ipsa libero voluptatem?
-        </p> */}
       </div>
       <div className="right-column" id="productSection">
         <ProductDetails
